436647616a00d702279da5ed214eb2ba6358cb24
abelanaya <abelanaya97@gmail.com>
2024-04-03 00:44:41 +0200

    refactor(db): Trim db creation script and improve schemas

--- queries/create_db.sql
+++ queries/create_db.sql
@@ -4,31 +4,48 @@ CREATE DATABASE rest_db;
 
 USE rest_db;
 
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations (
+	location_id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    location_name VARCHAR(255) NOT NULL,
+    location_type ENUM ("Building", "Room", "Outdoor", "Cargo bay") NOT NULL,
+    device_count INT DEFAULT 0
+);
+
 DROP TABLE IF EXISTS devices;
 
 CREATE TABLE devices (
-	serial_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type ENUM("Sensor", "Actuator") NOT NULL,
+	serial_number BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    device_name VARCHAR(255) NOT NULL,
+    device_type ENUM("Sensor", "Actuator") NOT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT (NOW())
 );
 
-INSERT INTO devices(name, type) VALUES
-("Light sensor", "Sensor"),
-("Fan", "Actuator"),
-("Light sensor", "Sensor");
+DROP TABLE IF EXISTS device_location;
 
-CREATE TABLE locations (
-	location_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type ENUM (
-		"Dam",
-		"Nuclear Power Plant",
-		"Farm"
-    ) NOT NULL
+CREATE TABLE device_location (
+	device_sn BINARY(16) NOT NULL PRIMARY KEY,
+    location_id BINARY(16) NOT NULL,
+    FOREIGN KEY (device_sn) REFERENCES devices(serial_number),
+    FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );
 
-INSERT INTO locations(name, type) VALUES
-("Paris Water Reserve", "Dam"),
-("London Electric", "Nuclear Power Plant"),
-("Berlin Ecologic", "Farm");
+/* Device count per location trigger */
+DELIMITER $$
+CREATE TRIGGER increase_device_count
+AFTER INSERT ON device_location
+FOR EACH ROW
+BEGIN
+	UPDATE locations SET device_count = device_count + 1
+	WHERE location_id = NEW.location_id;
+END $$
+
+DELIMITER $$
+CREATE TRIGGER decrease_device_count
+AFTER DELETE ON device_location
+FOR EACH ROW
+BEGIN
+	UPDATE locations SET device_count = device_count - 1
+	WHERE location_id = OLD.location_id;
+END $$
