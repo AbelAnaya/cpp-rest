96506cb6a29dba53b96f80708e880a091ca1607f
abelanaya <abelanaya97@gmail.com>
2024-04-09 01:00:50 +0200

    feat(sw): Implement device management queries

--- src/SQL_Client/SQL_Client.cpp
+++ src/SQL_Client/SQL_Client.cpp
@@ -1,4 +1,8 @@
 /**
+ * SQL Connection manager.
+ *
+ * This class manages SQL connection to mySQL database and handles REST API SQL requests.
+ * Implemented as Singleton
  *
  **/
 
@@ -8,12 +12,102 @@
 #include <memory>
 
 
+/********************************
+ * Static functions definition  *
+ ********************************/
+
+static void _add_int_filter_to_query
+(
+    std::string& query,
+    const crow::request& req,
+    const std::string& db_member,
+    const std::string& api_param,
+    int& n_params
+)
+{
+    char* param = req.url_params.get(api_param);
+
+    if (param != nullptr) {
+        query += db_member + "=" + param;
+        if (n_params > 1) {
+            query += " AND ";
+            n_params--;
+        }
+    }
+}
+
+
+static void _add_string_filter_to_query
+(
+    std::string& query,
+    const crow::request& req,
+    const std::string& db_member,
+    const std::string& api_param,
+    int& n_params
+)
+{
+    char* param = req.url_params.get(api_param);
+
+    if (param != nullptr) {
+        query += db_member + "=" + "\"" + param + "\"";
+        if (n_params > 1) {
+            query += " AND ";
+            n_params--;
+        }
+    }
+}
+
+
+static void _add_date_filter_to_query
+(
+    std::string& query,
+    const crow::request& req,
+    int& n_params
+)
+{
+    char* start_date = req.url_params.get("creationDateStart");
+    char* end_date = req.url_params.get("creationDateEnd");
+
+    if ((start_date != nullptr) && (end_date != nullptr)) {
+        query += (std::string) "creation_date BETWEEN \"" + start_date + "\" AND \"" + end_date + "\"";
+        if (n_params > 2) {
+            query += " AND ";
+            n_params -= 2;
+        }
+    }
+}
+
+
+static bool _check_id_exists
+(
+    sql::Connection* conn,
+    int id,
+    const std::string& table,
+    const std::string& column
+)
+{
+    bool rc;
+
+    std::unique_ptr<sql::Statement> stmt(conn->createStatement());
+
+    std::string check_query = "SELECT * FROM " + table + " WHERE " + column + " = " + std::to_string(id);
+    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery(check_query));
+
+    res->rowsCount() > 0 ? rc = true : rc = false;
+
+    return rc;
+}
+
+
+/*****************************************
+ * Public API Class functions definition *
+ *****************************************/
+
 SQLClient::SQLClient()
 {
     driver = get_driver_instance();
     con = driver->connect("localhost:3306", "root", "2515");
 
-    // Connect to the MySQL rest_db database
     con->setSchema("rest_db");
 }
 
@@ -31,24 +125,193 @@ SQLClient& SQLClient::getInstance()
     return sql_client;
 }
 
-void SQLClient::updateQuery()
+
+crow::status SQLClient::postDevice(const crow::json::rvalue& parameters)
 {
+    std::string query = "INSERT INTO devices VALUES("
+                        + std::to_string(parameters["serialNumber"].i()) + ", \""
+                        + (std::string) parameters["name"].s() + "\", \""
+                        + (std::string) parameters["type"].s() + "\", "
+                        + "NOW())";
+
     std::unique_ptr<sql::Statement> stmt(con->createStatement());
-    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery(
-        "UPDATE device_location SET ");
+
+    bool exec_status = false;
+
+    try
+    {
+        stmt->execute(query);
+
+        exec_status = true;
+    }
+    catch (sql::SQLException &e)
+    {
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << " (" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+
+        exec_status = false;
+    }
+
+    if ((exec_status) && (parameters.has("locationId")))
+    {
+        const bool loc_exists = _check_id_exists(con, parameters["locationId"].i(), "locations", "location_id");
+
+        if (loc_exists)
+        {
+            // Insert relation in device_location table
+            stmt->execute("INSERT INTO device_location VALUES(" + std::to_string(parameters["serialNumber"].i())
+                                                                + ", "
+                                                                + std::to_string(parameters["locationId"].i())
+                                                                + ")");
+            exec_status = true;
+        }
+        else
+        {
+            exec_status = false;
+        }
+    }
+
+    return exec_status ? crow::status::OK : crow::status::BAD_REQUEST;
 }
 
-void SQLClient::selectQuery()
-{
-    std::unique_ptr<sql::Statement> stmt(con->createStatement());
-    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery(
-        "SELECT * FROM devices \
-         JOIN device_location ON serial_number = device_sn \
-         JOIN locations ON device_location.location_id = locations.location_id \
-        "));
 
-    while (res->next()) {
-        std::cout << "\t... MySQL replies: ";
-        std::cout << res->getString("device_name") << std::endl;
+crow::status SQLClient::deleteDevice(const std::string& table, const std::string& column, const std::string& id)
+{
+    std::string query = "DELETE FROM " + table + " WHERE " + column + " = " + id;
+    std::unique_ptr<sql::Statement> stmt(con->createStatement());
+
+    stmt->execute(query);
+
+    const int affectedRows = stmt->getUpdateCount();
+
+    return affectedRows > 0 ? crow::status::OK : crow::status::NOT_FOUND;
+}
+
+
+crow::status SQLClient::putDevice(int serial_number, const crow::json::rvalue& parameters)
+{
+    const int locationId = parameters["locationId"].i();
+    const bool device_exists = _check_id_exists(con, serial_number, "devices", "serial_number");
+    const bool location_exists = _check_id_exists(con, locationId, "locations", "location_id");
+
+    if (device_exists && location_exists)
+    {
+        const bool device_location_exists = _check_id_exists(con, serial_number, "device_location", "device_sn");
+
+        std::string query = "";
+
+        if (device_location_exists)
+        {
+            query += "UPDATE device_location SET location_id = " + std::to_string(locationId)
+                                            + " WHERE device_sn = " + std::to_string(serial_number);
+        }
+        else
+        {
+            query += "INSERT INTO device_location VALUES(" + std::to_string(serial_number)
+                                                           + "," + std::to_string(locationId) + ")";
+        }
+
+        std::unique_ptr<sql::Statement> stmt(con->createStatement());
+        stmt->execute(query);
+
+        const int affectedRows = stmt->getUpdateCount();
+
+        return affectedRows > 0 ? crow::status::OK : crow::status::BAD_REQUEST;
     }
+    else
+    {
+        return crow::status::BAD_REQUEST;
+    }
+
+}
+
+crow::response SQLClient::getDevice(const int id)
+{
+    std::string query = "SELECT serial_number, device_name, \
+     device_type, creation_date, locations.location_id, \
+     locations.location_name, locations.location_type FROM devices \
+     LEFT JOIN device_location ON serial_number = device_sn \
+     LEFT JOIN locations ON locations.location_id = device_location.location_id \
+     WHERE serial_number = " + std::to_string(id);
+
+    std::cout << "Query: " + query << std::endl;
+
+    std::unique_ptr<sql::Statement> stmt(con->createStatement());
+    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery(query));
+
+    if (res->rowsCount() > 0)
+    {
+        res->next();
+
+        crow::json::wvalue body_response({
+            {"serialNumber", res->getInt("serial_number")},
+            {"name", (std::string) res->getString("device_name")},
+            {"type", (std::string) res->getString("device_type")},
+            {"creationDate", (std::string) res->getString("creation_date")},
+            {"locationId", res->getInt("location_id")},
+            {"locationName", (std::string) res->getString("location_name")},
+            {"locationType", (std::string) res->getString("location_type")},
+        });
+
+        return crow::response(crow::status::OK, body_response);
+    }
+    else
+    {
+        return crow::response(crow::status::NOT_FOUND);
+    }
+}
+
+
+crow::response SQLClient::getAllDevices(const crow::request& req)
+{
+    std::string query = "SELECT serial_number, device_name, \
+     device_type, creation_date, locations.location_id, \
+     locations.location_name, locations.location_type FROM devices \
+     LEFT JOIN device_location ON serial_number = device_sn \
+     LEFT JOIN locations ON locations.location_id = device_location.location_id";
+
+    int n_params = req.url_params.keys().size();
+
+    if (n_params != 0) {
+        query += " WHERE ";
+
+        _add_string_filter_to_query(query, req, "device_name", "name", n_params);
+        _add_string_filter_to_query(query, req, "device_type", "type", n_params);
+        _add_int_filter_to_query(query, req, "locations.location_id", "locationId", n_params);
+        _add_string_filter_to_query(query, req, "locations.location_name", "locationName", n_params);
+        _add_string_filter_to_query(query, req, "locations.location_type", "locationType", n_params);
+        _add_date_filter_to_query(query, req, n_params);
+    }
+
+    std::cout << "Query: " + query << std::endl;
+
+    std::unique_ptr<sql::Statement> stmt(con->createStatement());
+    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery(query));
+
+    std::vector<crow::json::wvalue> body_response;
+    int iteration = 0;
+
+    while(res->next())
+    {
+        crow::json::wvalue device_item(
+            {
+                {"serialNumber", res->getInt("serial_number")},
+                {"name", (std::string) res->getString("device_name")},
+                {"type", (std::string) res->getString("device_type")},
+                {"creationDate", (std::string) res->getString("creation_date")},
+                {"locationId", res->getInt("location_id")},
+                {"locationName", (std::string) res->getString("location_name")},
+                {"locationType", (std::string) res->getString("location_type")},
+            }
+        );
+
+        body_response.push_back(device_item);
+
+        iteration++;
+    }
+
+    return (crow::json::wvalue) body_response;
 }
--- src/SQL_Client/SQL_Client.hpp
+++ src/SQL_Client/SQL_Client.hpp
@@ -8,6 +8,7 @@
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
 #include <cppconn/resultset.h>
+#include <crow.h>
 
 class SQLClient
 {
@@ -28,7 +29,11 @@ public:
 
     static SQLClient& getInstance();
 
-    void selectQuery();
+    crow::status postDevice(const crow::json::rvalue& parameters);
+    crow::status deleteDevice(const std::string& table, const std::string& column, const std::string& id);
+    crow::status putDevice(int serial_number, const crow::json::rvalue& parameters);
+    crow::response getDevice(const int id);
+    crow::response getAllDevices(const crow::request& req);
 };
 
 
